d606aa011d96535f69dbf34349c6e7a9afdddc65
Hui-Hong You <hiroshi@ghostsinthelab.org>
2026-02-10 11:55:24 +0800

    Commit NetBSD source code CVS tree
    Signed-off-by: Hui-Hong You <hiroshi@ghostsinthelab.org>

--- envsetup.sh
+++ envsetup.sh
@@ -3,3 +3,4 @@
 export LABSROOT=$(pwd)
 export LABS_M0_ROOT=$(pwd)/labs/0-build_edk2
 export LABS_M1_ROOT=$(pwd)/labs/1-boot_netbsd_from_uefi
+export LABS_M2_ROOT=$(pwd)/labs/2-build_netbsd_for_rpi1b
